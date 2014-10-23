3da30b8a3f1f34bd97d0d6bc42069639e63c20ac
Kong king sin <fishkingsin@gmail.com>
2014-10-24 00:02:09 +0800

    myo sdk beta 5
    myo-sdk-mac-beta5
    and simple example
    single myo only

---
+++ example/src/ofApp.cpp
@@ -0,0 +1,102 @@
+#include "ofApp.h"
+#define SAMPLE_SIZE 512
+//--------------------------------------------------------------
+void ofApp::setup(){
+    m_myo.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    m_myo.update();
+    m_myo.printDebug();
+    MyoFrameOrientationData orientationData = m_myo.getCurrFrameOrientionData();
+    
+    roll.push_back(orientationData.euler_roll);
+	pitch.push_back(orientationData.euler_pitch);
+	yaw.push_back(orientationData.euler_yaw);
+    if(pitch.size()>SAMPLE_SIZE)
+    {
+        pitch.erase(pitch.begin());
+    }
+    if(yaw.size()>SAMPLE_SIZE)
+    {
+        yaw.erase(yaw.begin());
+    }
+    if(roll.size()>SAMPLE_SIZE)
+    {
+        roll.erase(roll.begin());
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    float half_H = ofGetHeight()*0.5;
+    float maxH = ofGetHeight()*0.25;
+    ofPushStyle();
+    ofSetHexColor(0x101010);
+    for(unsigned int i=1;i<pitch.size();i++){
+        
+        ofLine(i-1,pitch[i-1]*maxH+half_H,i,pitch[i]*maxH+half_H);
+    }
+    ofPopStyle();
+    ofPushStyle();
+    ofSetColor(ofColor::green);
+    for(unsigned int i=1;i<yaw.size();i++){
+        ofLine(i-1,yaw[i-1]*maxH+half_H,i,yaw[i]*maxH+half_H);
+    }
+    ofPopStyle();
+    
+    ofPopStyle();
+    ofPushStyle();
+    ofSetColor(ofColor::red);
+    for(unsigned int i=1;i<roll.size();i++){
+        ofLine(i-1,roll[i-1]*maxH+half_H,i,roll[i]*maxH+half_H);
+    }
+    ofPopStyle();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
--- src/ofxMyo.cpp
+++ src/ofxMyo.cpp
@@ -1,6 +1,6 @@
 #include "ofxMyo.h"
 
-ofxMyo::ofxMyo() : m_hub(nullptr)
+ofxMyo::ofxMyo() : m_hub(NULL)
 {
 }
 
@@ -21,7 +21,7 @@ void ofxMyo::setup()
 		// value in milliseconds. In this case we will try to find a Myo for 10 seconds, and if that fails, the function
 		// will return a null pointer.
 		const int wait_num_ms = 10000;
-		myo::Myo* myo = m_hub->waitForAnyMyo(wait_num_ms);
+		myo::Myo* myo = m_hub->waitForMyo(wait_num_ms);
 
 		// If waitForAnyMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
 		if (!myo) 
@@ -30,14 +30,14 @@ void ofxMyo::setup()
 		}
 
 		// We've found a Myo, let's output its MAC address.
-		std::cout << "Connected to " << myo->macAddressAsString() << "." << std::endl << std::endl;
+//		std::cout << "Connected to " << myo->macAddressAsString() << "." << std::endl << std::endl;
 
 		// Check if the Myo has been trained. If it hasn't been trained, it will still function, but it won't generate any
 		// pose events.
-		if (!myo->isTrained()) 
-		{
-			throw std::runtime_error("Myo has not been trained. Do you need to run the trainer?");
-		}
+//		if (!myo->isTrained()) 
+//		{
+//			throw std::runtime_error("Myo has not been trained. Do you need to run the trainer?");
+//		}
 
 		// Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
 		// Hub::run() to send events to all registered device listeners.
@@ -75,7 +75,7 @@ void ofxMyo::release()
 	if ( m_hub ) 
 	{
 		delete m_hub;
-		m_hub = nullptr;
+		m_hub = NULL;
 	}
 }
 
